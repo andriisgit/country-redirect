26aadf6eaec092ec2a84cd8fa1de93d93fcac13d
andriisgit <sichko.andrii@gmail.com>
2019-10-26 13:00:49 +0300

    add url validation from user's input

--- geo-redirect.php
+++ geo-redirect.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: geo-redirect
-Plugin URI:
+Plugin URI: https://github.com/andriisgit/geo-redirect
 Description: Simple to use plugin for redirection depending visitor's country
 Version: 1.0
 */
@@ -41,7 +41,7 @@ register_activation_hook( __FILE__, 'gr_activation' );
  * Deactivate the plugin
  * ------------------------------------------------------------------------ */
 function gr_deactivation() {
-
+    // empty
 }
 
 register_deactivation_hook( __FILE__, 'gr_deactivation' );
@@ -53,7 +53,8 @@ function gr_activation_notice() {
 	if ( get_transient( 'gr-activation-notice' ) ) {
 		?>
         <div class="updated notice is-dismissible">
-            <p>Geo Redirect activated. You can go to</p>
+            <p>Geo Redirect <?php _e ('activated') ?>.</p>
+            <p><?php _e('You can set redirection') ?> <a href="options-general.php?page=geo-redirect-options"><?php _e('Settings') ?> - Geo Redirect</a></p>
         </div>
 		<?php
 		/* Delete transient, only display this notice once. */
@@ -65,10 +66,18 @@ add_action( 'admin_notices', 'gr_activation_notice' );
 
 
 /* ------------------------------------------------------------------------ *
+ * Localization
+ * ------------------------------------------------------------------------ */
+add_action( 'plugins_loaded', function () {
+	load_plugin_textdomain( 'grl10n', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+});
+
+
+/* ------------------------------------------------------------------------ *
  * Connect JS and CSS
  * ------------------------------------------------------------------------ */
 function gr_enqueue() {
-
+	// empty
 }
 
 add_action( 'wp_enqueue_scripts', 'gr_enqueue' );
@@ -89,20 +98,18 @@ function gr_admin_page() {
 
         <h2>Geo Redirect Options</h2>
 
-        <!-- Make a call to the WordPress function for rendering errors when settings are saved. -->
-		<?php settings_errors(); ?>
-
 		<?php
+		// Make a call to the WordPress function for rendering errors when settings are saved.
+		settings_errors();
+
 		$active_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : 'engine_options';
 		?>
 
         <h2 class="nav-tab-wrapper">
             <a href="?page=geo-redirect-options&tab=engine_options"
-               class="nav-tab <?php echo $active_tab == 'engine_options' ? 'nav-tab-active' : ''; ?>">Engine
-                Settings</a>
+               class="nav-tab <?php echo $active_tab == 'engine_options' ? 'nav-tab-active' : ''; ?>">Engine Settings</a>
             <a href="?page=geo-redirect-options&tab=redirect_options"
-               class="nav-tab <?php echo $active_tab == 'redirect_options' ? 'nav-tab-active' : ''; ?>">Redirect
-                Settings</a>
+               class="nav-tab <?php echo $active_tab == 'redirect_options' ? 'nav-tab-active' : ''; ?>">Redirect Settings</a>
         </h2>
 
         <form method="post" action="options.php">
@@ -134,36 +141,36 @@ function gr_initialize_options() {
 		[
 			'id'    => 'gr_engine_sxgeo',
 			'title' => 'SxGeo (local)',
-			'label' => 'Use SxGeo library to determine user\'s country. For additional info about the library, check https://sypexgeo.net'
+			'label' => __('Use') . ' SxGeo ' . __('library to determine user\'s country. For additional info about the library, check') . ' https://sypexgeo.net'
 		],
 		[
 			'id'    => 'gr_engine_geoip2',
 			'title' => 'GeoIp2 (local)',
-			'label' => 'Use GeoIP2 library to determine user\'s country. For additional info about the library, check https://dev.maxmind.com/geoip/'
+			'label' => __('Use') . ' GeoIP2 library to determine user\'s country. For additional info about the library, check https://dev.maxmind.com/geoip/'
 		],
 		[
 			'id'    => 'gr_engine_geoip',
 			'title' => 'GEOIP DB (remote)',
-			'label' => 'Use GEOIP DB API to determine user\'s country. For additional info about the library, check https://geoip-db.com'
+			'label' => __('Use') . ' GEOIP DB API to determine user\'s country. For additional info about the library, check https://geoip-db.com'
 		],
 		[
 			'id'    => 'gr_engine_ipapi',
 			'title' => 'ip-api (remote)',
-			'label' => 'Use ip-api API to determine user\'s country. For additional info about the library, check http://ip-api.com'
+			'label' => __('Use') . ' ip-api API to determine user\'s country. For additional info about the library, check http://ip-api.com'
 		]
 	];
 
 
 	add_settings_section(
 		'gr_engine_settings',         // ID used to identify this section and with which to register options
-		'Engine Settings',                  // Title to be displayed on the administration page
+		__('Engine Settings'),                  // Title to be displayed on the administration page
 		'gr_engine_settings_callback', // Callback used to render the description of the section
 		'engine'                           // Page on which to add this section of options
 	);
 
 	add_settings_section(
 		'gr_redirect_settings',         // ID used to identify this section and with which to register options
-		'Redirect Settings',                  // Title to be displayed on the administration page
+		__('Redirect Settings'),                  // Title to be displayed on the administration page
 		'gr_redirect_settings_callback', // Callback used to render the description of the section
 		'redirect'                           // Page on which to add this section of options
 	);
@@ -179,7 +186,7 @@ function gr_initialize_options() {
 	foreach ( $SxGeo->id2iso as $code ) {
 		if ( ! empty( $code ) ) {
 			add_settings_field( 'gr_redirect_' . $code, $code, 'gr_toggle_redirect', 'redirect', 'gr_redirect_settings', [ $code ] );
-			register_setting( 'redirect', 'gr_redirect_' . $code );
+			register_setting( 'redirect', 'gr_redirect_' . $code, 'gr_validate_url' );
 		}
 	}
 
@@ -201,6 +208,14 @@ function gr_redirect_settings_callback() {
 	echo '<p>To see complete list of codes, check Wikipedia page: https://en.wikipedia.org/wiki/ISO_3166-1</p>';
 }
 
+/* ------------------------------------------------------------------------ *
+ * Validating
+ * ------------------------------------------------------------------------ */
+function gr_validate_url($input) {
+	$output = esc_url_raw($input);
+
+	return apply_filters( 'gr_validate_url', $output, $input );
+}
 
 /* ------------------------------------------------------------------------ *
  * Field Callbacks
@@ -218,7 +233,7 @@ function gr_toggle_engine( $args ) {
 function gr_toggle_redirect( $args ) {
 
 	$option_name = 'gr_redirect_' . $args[0];
-
+$Z=get_option( $option_name );
 	$html = '<input type="text" id="' . $option_name . '" name="' . $option_name . '" value="' . get_option( $option_name ) . '" class="regular-text code"/>';
 	$html .= '<label for="' . $option_name . '"> Put here redirection URL for ' . $args[0] . '</label>';
 
@@ -235,26 +250,26 @@ add_action( 'template_redirect', function () {
 		$ip = $_SERVER['REMOTE_ADDR'];
 		//$ip = '46.133.64.2'; // O mobile
 		$ip = '85.209.44.123'; // I work computer
-        //$ip = '178.133.73.146'; // I work mobile
+		//$ip = '178.133.73.146'; // I work mobile
 		$redirect = null;
 
 		// Checking country using local SxGeo
 		if ( get_option( 'gr_engine_sxgeo' ) ) {
 			include_once 'engine' . DIRECTORY_SEPARATOR . 'sxgeo.php';
 			$country = sxgeo( $ip );
-            if ($country) {
-	            $url = get_option( 'gr_redirect_' . $country );
-	            if ( wp_http_validate_url( $url ) ) {
-		            $redirect = $url;
-	            }
-            }
+			if ( $country ) {
+				$url = get_option( 'gr_redirect_' . $country );
+				if ( wp_http_validate_url( $url ) ) {
+					$redirect = $url;
+				}
+			}
 		}
 
 		// Checking country using local GeoIp2
-		if ( !is_null($redirect) && get_option( 'gr_engine_geoip2' ) ) {
+		if ( ! is_null( $redirect ) && get_option( 'gr_engine_geoip2' ) ) {
 			include_once 'engine' . DIRECTORY_SEPARATOR . 'geoip2.php';
 			$country = geoip2( $ip );
-			if ($country) {
+			if ( $country ) {
 				$url = get_option( 'gr_redirect_' . $country );
 				if ( wp_http_validate_url( $url ) ) {
 					$redirect = $url;
@@ -266,7 +281,7 @@ add_action( 'template_redirect', function () {
 
 		// Checking country using remote ip-api
 
-		if ( !is_null($redirect) ) {
+		if ( ! is_null( $redirect ) ) {
 			wp_redirect( $redirect );
 			exit;
 		}
@@ -279,11 +294,13 @@ function in_country_code( $country ) {
 	$SxGeo = new SxGeo( $path . '.dat' );
 	$countries = $SxGeo->id2iso;
 
-	if ($countries[0] == '')
-		array_shift($countries);
+	if ( $countries[0] == '' ) {
+		array_shift( $countries );
+	}
 
-    if ( in_array( $country, $countries ) )
+	if ( in_array( $country, $countries ) ) {
 		return true;
+	}
 
 	return false;
 
